ad8c4393625b1c886b541a1a1870c6b05afa912f
Swayam Shah <swayamshah66@gmail.com>
2025-01-23 23:57:38 +0530

    fixes minor

--- src/Components/ProposalPage.jsx
+++ src/Components/ProposalPage.jsx
@@ -5,7 +5,7 @@ const ProposalPage = () => {
   return (
     <div className="min-h-screen bg--100% text-white flex items-center justify-center">
       <div className="w-full max-w-4x rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">Submit Your Proposal</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center"style={{ color: '#814fde' }}>Proposal Submission</h1>
         <ProposalForm />
       </div>
     </div>
