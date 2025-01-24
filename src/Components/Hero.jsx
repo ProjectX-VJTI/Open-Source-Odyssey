da04d7f32906ca653f4465217a31401fa95b845f
drchavan99 <chavandipesh399@gmail.com>
2025-01-24 18:29:29 +0530

    achievements-enhanced

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="relative bg-transparent min-h-screen w-full flex">
-    <div className="flex-1 bg-transparent flex items-center justify-center">
-        <div className="gp-10 rounded-lg text-left">
-            <h1 className="text-bounce text-[10rem] leading-none font-mono tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600">ProjectX</h1>
-            <p className="text-bounce text-5xl leading-relaxed font-mono tracking-wide text-transparent text-center bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 mt-5">Innovating the future</p>
+            <div className="relative bg-transparent min-h-screen w-full flex">
+        <div className="flex-1 bg-transparent flex items-center justify-center">
+            <div className="gp-10 rounded-lg text-left">
+                <h1 className="text-bounce text-[10rem] leading-none font-mono tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600">ProjectX</h1>
+                <p className="text-bounce text-5xl leading-relaxed font-mono tracking-wide text-transparent text-center bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 mt-5">Innovating the future</p>
+            </div>
         </div>
-    </div>
     <svg
         className="absolute bottom-0 left-0 right-0 z-10 opacity-100"
         viewBox="0 0 1440 320"
