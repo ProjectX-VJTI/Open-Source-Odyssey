722277b2066c38e6f82dc7f39daf0ac8749f59d2
shivraj0124 <shivrajkolwankar0124@gmail.com>
2025-01-24 17:18:40 +0530

    'lazy-load-image'

--- src/Components/CircularCard.jsx
+++ src/Components/CircularCard.jsx
@@ -1,40 +1,31 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
-import myHook from "./Context";
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 const CircularCard = ({ image, name, organization, link }) => {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
-  const { siteMode } = myHook();
+
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="no-underline"
-    >
+    <a href={link} target="_blank" rel="noopener noreferrer" className="no-underline">
       <animated.div
         style={props}
         className="flex text-center font-mono flex-col items-center p-6 shadow-lg max-w-xs bg-transparent rounded-lg transform hover:scale-105 transition-transform duration-200 ease-in-out"
       >
         <div className="w-32 h-32 rounded-full overflow-hidden shadow-lg">
-          <img src={image} alt={name} className="w-full h-full object-cover" />
+          <LazyLoadImage
+            src={image}
+            alt={name}
+            effect="blur"
+            className="w-full h-full object-cover"
+          />
         </div>
-        <h3
-          className={`mt-4 text-xl font-bold ${
-            siteMode === "dark" ? "text-purple-200" : "text-purple-500"
-          } `}
-        >
-          {name}
-        </h3>
-        <p
-          className={`mt-2 ${
-            siteMode === "dark" ? "text-cyan-200" : "text-cyan-500"
-          }  `}
-        >
-          {organization}
-        </p>
+        <h3 className="mt-4 text-xl font-bold text-purple-200">{name}</h3>
+        <p className="mt-2 text-cyan-200">{organization}</p>
       </animated.div>
     </a>
   );
 };
 
 export default CircularCard;
+  
